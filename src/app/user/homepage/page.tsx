2b106a18af60ad4e4d9c0ee2bb16f698f1df9292
Sagar Sen <sagarsen.dev@gmail.com>
2024-06-28 12:32:10 +0530

    user/homepage is under development

--- src/app/user/homepage/page.tsx
+++ src/app/user/homepage/page.tsx
@@ -1,14 +1,45 @@
 "use client"
+import CircularProgressIndicator from '@/components/CircularProgressIndicator'
 import fetchUserData from '@/utils/fetchUserData'
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
+import { toast } from 'sonner'
+
+interface UserData{
+  // Structure the interface
+}
 
 const page = () => {
-  useEffect(() => {
-    fetchUserData()
+  const [loading, setLoading] = useState(false)
+  const [user, setUser] = useState()
+
+  useEffect(() => {  
+    async function getUserdata() {
+      try{
+        setLoading(true)
+        const fetchedUser = await fetchUserData()
+        setUser(fetchedUser)
+      } catch (err:any) {
+        toast.error(err.message)
+      } finally{
+        setLoading(false)
+      }
+    }
+    getUserdata()
   }, [])
   
+  if (loading) return <>
+  <div className='w-full h-[90vh] flex justify-center items-center'>
+    <CircularProgressIndicator />
+  </div>
+</>
+
   return (
-    <div>Homepage</div>
+    <>
+    <pre>
+      User Data:
+      {JSON.stringify(user, null, 2)}
+    </pre>
+    </>
   )
 }
 
