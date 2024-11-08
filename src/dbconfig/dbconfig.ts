65b97d8a743cbe711540aeda4c23955057009544
Sagar Sen <sagarsen.dev@gmail.com>
2024-11-08 23:32:46 +0530

    env updated

--- src/dbconfig/dbconfig.ts
+++ src/dbconfig/dbconfig.ts
@@ -2,27 +2,29 @@ import mongoose from "mongoose";
 import { toast } from "sonner";
 
 type ConnectionObject = {
-    isConnected?: number
-}
+  isConnected?: number;
+};
 
-const connection : ConnectionObject = {}
+const connection: ConnectionObject = {};
 
-export default async function connectToDb(){
-  if(connection.isConnected){
-      // console.log("Already connected to database.");
-      return
-    }
+export default async function connectToDb() {
+  if (connection.isConnected) {
+    // console.log("Already connected to database.");
+    return;
+  }
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI!)
-    connection.isConnected = db.connections[0].readyState
-    mongoose.connection.on('connection', () => {
+    const db = await mongoose.connect(
+      "mongodb+srv://sagarsendev:sagarsendev@styleloom.otwqr.mongodb.net/"
+    );
+    connection.isConnected = db.connections[0].readyState;
+    mongoose.connection.on("connection", () => {
       // ! This callback can be used for testing / implementing middlewares
-    })
-    mongoose.connection.on('error', () => {
-        toast.error("Error while connecting the database")
-    })
-  } catch (error : any) {
-    toast.error(error.message)
-    process.exit(1)
+    });
+    mongoose.connection.on("error", () => {
+      toast.error("Error while connecting the database");
+    });
+  } catch (error: any) {
+    toast.error(error.message);
+    process.exit(1);
   }
 }
